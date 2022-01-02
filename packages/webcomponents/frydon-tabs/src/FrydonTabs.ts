6c6a7a25dc0c3495c776917c5d943f7bd6d4d712
frydonlee <frydon.mail@gmail.com>
2022-01-02 23:33:04 +0100

    fix(frydon-tabs): correct import frydon-icon

--- packages/webcomponents/frydon-tabs/src/FrydonTabs.ts
+++ packages/webcomponents/frydon-tabs/src/FrydonTabs.ts
@@ -2,7 +2,8 @@ import { html, css, LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
 import '@vaadin/tabs';
 //import '@frydonlee/frydon-icon/frydon-icon.js';
-import '../../../frydon-icon/dist/src/frydon-icon.js'
+import { FrydonIcon } from '@frydonlee/frydon-icon';
+//import '../../../frydon-icon/dist/src/frydon-icon.js'
 import { Tab } from './interface.js';
 
 export class FrydonTabs extends LitElement {
@@ -13,6 +14,12 @@ export class FrydonTabs extends LitElement {
       color: var(--frydon-tabs-text-color, #000);
     }
   `;
+
+  constructor() {
+    super();
+    if(customElements.get('frydon-icon') === undefined)
+      window.customElements.define('frydon-icon', FrydonIcon);
+  }
 
   @property({ type: Array }) tabs: Array<Tab> = [];
 
